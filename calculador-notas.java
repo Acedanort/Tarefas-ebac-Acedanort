53a89c23817591fa63d3bee7c595f11985df28ab
Acedanort <rodriguessarthur@gmail.com>
2025-08-26 17:26:55 -0300

    caculadora para notas

---
+++ calculador-notas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ResultadoAluno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite as quatro notas do aluno:");
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+        double nota4 = scanner.nextDouble();
+        
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+        System.out.println("Média: " + media);
+        
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else if (media >= 5) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+        
+        scanner.close();
+    }
+}
