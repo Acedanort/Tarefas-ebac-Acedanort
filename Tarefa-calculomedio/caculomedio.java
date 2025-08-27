023f5da48bea0f7186fb3c681e1d67714be37cb6
Acedanort <rodriguessarthur@gmail.com>
2025-08-27 16:53:29 -0300

    caculadora media

---
+++ Tarefa-calculomedio/caculomedio.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite as quatro notas:");
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+        double nota4 = scanner.nextDouble();
+        
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+        System.out.println("A média das notas é: " + media);
+        
+        scanner.close();
+    }
+}
